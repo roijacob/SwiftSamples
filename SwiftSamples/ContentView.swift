841fac4786de75dd47a5992cdde879040e38dbd0
roijacob <roijac2b@outlook.com>
2024-11-21 23:16:50 +0800

    Initial Commit

---
+++ SwiftSamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSamples
+//
+//  Created by Roi Jacob on 11/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSamples/SwiftSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSamplesApp.swift
+//  SwiftSamples
+//
+//  Created by Roi Jacob on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
